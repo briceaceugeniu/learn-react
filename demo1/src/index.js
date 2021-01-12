f9c7c4ca0cc2c177e11cae6235c4294b6a7f1f98
Briceac Eugeniu <eb@h4h.cc>
2021-01-12 17:58:31 +0100

    go home

--- demo1/src/index.js
+++ demo1/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from "./CommentDetail";
+
+let txt3 = "I am out!";
+
+const Comment =  () => {
+    let txt2 = "What did you say?!";
+  return (
+    <div className="ui container comments">
+      <CommentDetail author="Samuel" text="Hello World" ts={getTime()} avatarSrc="https://source.unsplash.com/random"/> <br/>
+      <CommentDetail author="Bobby" text={txt2} ts={getTime()} avatarSrc="https://source.unsplash.com/random"/> <br/>
+      <CommentDetail author="Luisa" text={txt3} ts={getTime()} avatarSrc="https://source.unsplash.com/random"/>
+    </div>
+  );
+};
+
+
+function getTime() {
+    return (new Date()).toLocaleTimeString()
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Comment />,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
