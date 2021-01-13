deeefc4ed11a73e478647aee61a77c8afb91adfc
Briceac Eugeniu <eb@h4h.cc>
2021-01-13 10:45:06 +0100

    add demo2-seasons

--- demo1/src/CommentDetail.js
+++ demo1/src/CommentDetail.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const CommentDetail = (props) => {
-    return (
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={props.avatarSrc} />
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    {props.author}
-                </a>
-                <div className="metadata">
-                    <span className="date">{props.ts}</span>
-                </div>
-                <div className="text">{props.text}</div>
-            </div>
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.avatarSrc} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">{props.ts}</span>
         </div>
-    );
+        <div className="text">{props.text}</div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentDetail;
--- demo1/src/index.js
+++ demo1/src/index.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 let txt3 = "I am out!";
 
-const Comment =  () => {
-    let txt2 = "What did you say?!";
+const Comment = () => {
+  let txt2 = "What did you say?!";
   return (
     <div className="ui container comments">
-      <CommentDetail author="Samuel" text="Hello World" ts={getTime()} avatarSrc="https://source.unsplash.com/random"/> <br/>
-      <CommentDetail author="Bobby" text={txt2} ts={getTime()} avatarSrc="https://source.unsplash.com/random"/> <br/>
-      <CommentDetail author="Luisa" text={txt3} ts={getTime()} avatarSrc="https://source.unsplash.com/random"/>
+      <ApprovalCard>
+        <p>Are you sure you want to continue?</p>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Samuel"
+          text="Hello World"
+          ts={getTime()}
+          avatarSrc="https://source.unsplash.com/random"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Bobby"
+          text={txt2}
+          ts={getTime()}
+          avatarSrc="https://source.unsplash.com/random"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Luisa"
+          text={txt3}
+          ts={getTime()}
+          avatarSrc="https://source.unsplash.com/random"
+        />
+      </ApprovalCard>
     </div>
   );
 };
 
-
 function getTime() {
-    return (new Date()).toLocaleTimeString()
+  return new Date().toLocaleTimeString();
 }
 
-
-ReactDOM.render(
-  <Comment />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Comment />, document.querySelector("#root"));
