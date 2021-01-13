d87d136fbf095a8b9d021b2570f2d965dc58c51c
Briceac Eugeniu <eb@h4h.cc>
2021-01-13 16:26:36 +0100

    demo2 doce

--- demo2-seasons/src/App.js
+++ demo2-seasons/src/App.js
@@ -1,51 +1,24 @@
 import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 
-let currentMonth = new Date().getMonth() + 1;
-let hemisphere = whatHemisphere();
-let season = getSeason(currentMonth, hemisphere);
+class App extends React.Component {
+  state = { lat: null };
 
-const App = () => {
-  return <SeasonDisplay icon={getIcon(season)} text={getText(season)} />;
-};
+  componentDidMount() {
+    // geolocation
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ lat: err.message });
+      }
+    );
+  }
 
-// geolocation
-window.navigator.geolocation.getCurrentPosition(
-  (position) => console.log(position),
-  (err) => console.log(err)
-);
-
-function whatHemisphere() {
-  let y = new Date();
-  if (y.getTimezoneOffset == undefined) return null;
-  y = y.getFullYear();
-  let jan = -new Date(y, 0, 1, 0, 0, 0, 0).getTimezoneOffset();
-  let jul = -new Date(y, 6, 1, 0, 0, 0, 0).getTimezoneOffset();
-  let diff = jan - jul;
-  if (diff < 0) return "N";
-  if (diff > 0) return "S";
-  return null;
-}
-
-function getSeason(month, hemisphere) {
-  if (month >= 3 && month < 10) {
-    if (hemisphere === "N") return "S";
-    else return "W";
-  } else if (hemisphere === "N") return "W";
-  else return "S";
-}
-
-function getIcon(season) {
-  if (season === "W") {
-    return "https://www.123gif.de/gifs/winter/winterlandschaft-0042.gif";
-  } else
-    return "https://media.tenor.com/images/aae2269344e98521aa0e624323f4c549/tenor.gif";
-}
-
-function getText(season) {
-  if (season === "S") {
-    return "Sommer time!!";
-  } else return "Brr! Ist es kalt!";
+  render() {
+    return <SeasonDisplay lat={this.state.lat} />;
+  }
 }
 
 export default App;
--- demo2-seasons/src/SeasonDisplay.js
+++ demo2-seasons/src/SeasonDisplay.js
@@ -1,10 +1,43 @@
+import "./SeasonDisplay.css";
+
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Sommer time!!",
+    imgSrc:
+      "https://media.tenor.com/images/aae2269344e98521aa0e624323f4c549/tenor.gif",
+    icon: "huge sun icon",
+  },
+  winter: {
+    text: "Brr! Ist es kalt!",
+    imgSrc: "https://www.123gif.de/gifs/winter/winterlandschaft-0042.gif",
+    icon: "huge snowflake icon",
+  },
+};
+
+function getSeason(month, hemisphere) {
+  if (month >= 3 && month < 10) {
+    if (hemisphere === "N") return "summer";
+    else return "winter";
+  } else if (hemisphere === "N") return "winter";
+  else return "summer";
+}
+
+// Component
 const SeasonDisplay = (props) => {
+  let month = new Date().getMonth() + 1;
+  let hemisphere = props.lat >= 0 ? "N" : "S";
+  let season = getSeason(month, hemisphere);
+
+  let { text, imgSrc, icon } = seasonConfig[season];
+
   return (
     <div>
-      <p>{props.text}</p>
-      <img src={props.icon} alt="icon" />
+      <i className={`cost-icon-${season} ${icon}`} />
+      <i className={`cost-icon-${season} ${icon}`} />
+      <p className={`txt-${season}`}>{text}</p>
+      <img src={imgSrc} alt="season" />
     </div>
   );
 };
