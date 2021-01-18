2e9c9701ce0a9299ef3cc1fe5c97bf757c4a0e31
Briceac Eugeniu <eb@h4h.cc>
2021-01-18 17:50:38 +0100

    go home

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,32 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-{
-  /*const items = [*/
-}
-//   {
-//     id: 1,
-//     title: "Was ist React?",
-//     content: "React ist eine beliebte JavaScript framework.",
-//   },
-//   {
-//     id: 2,
-//     title: "Wie verwendet man React?",
-//     content: "Indem man erstellt React Components",
-//   },
-//   {
-//     id: 3,
-//     title: "Warum React",
-//     content: "Weil es cool ist!",
-//   },
-// ];
+const items = [
+  {
+    id: 1,
+    title: "Was ist React?",
+    content: "React ist eine beliebte JavaScript framework.",
+  },
+  {
+    id: 2,
+    title: "Wie verwendet man React?",
+    content: "Indem man erstellt React Components",
+  },
+  {
+    id: 3,
+    title: "Warum React",
+    content: "Weil es cool ist!",
+  },
+];
+
+const colorOptions = [
+  {
+    label: "Red",
+    value: "red",
+  },
+  {
+    label: "Blue",
+    value: "blue",
+  },
+  {
+    label: "Yellow",
+    value: "yellow",
+  },
+  {
+    label: "Green",
+    value: "green",
+  },
+];
 
 export default () => {
+  const [selected, setSelected] = useState(colorOptions[0]);
+
   return (
     <div>
       {/*<Accordion items={items} />*/}
-      <Search />
+      {/*<Search />*/}
+      <Dropdown
+        about={`Color`}
+        options={colorOptions}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
