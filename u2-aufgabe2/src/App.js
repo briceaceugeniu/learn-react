d56115271dbf12565c6d6256eba83e5e8aaa0641
Lehrling Eugen <28739569+briceaceugeniu@users.noreply.github.com>
2021-01-29 07:12:03 +0100

    aufgabe 2, first steps

--- u2-aufgabe2/src/App.js
+++ u2-aufgabe2/src/App.js
@@ -3,11 +3,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ui container">
       <h3 className={`ui brown header`}>Enter something...</h3>
-      <div className="ui big icon input">
+      <div className="ui input large active">
         <input type="text" placeholder="..." />
       </div>
+        <div className="ui grid">
+            <div className="four wide column">H</div>
+            <div className="four wide column">E</div>
+            <div className="four wide column">L</div>
+            <div className="four wide column">L</div>
+            <div className="four wide column">O</div>
+            <div className="four wide column">!</div>
+            <div className="four wide column">!</div>
+        </div>
     </div>
   );
 }
