0d61dc1511248a61b0ca2a6431894ed6b474a6df
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2021-02-10 19:11:00 +0100

    introduce very smart comment
    so, the programmer ask himself a very legitim question
    What the heck is this StrictMode?!?!?

--- u2-demo1/src/index.js
+++ u2-demo1/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./containers/App";
 
+// What the heck is React.StrictMode
 ReactDOM.render(
   <React.StrictMode>
     <App title={`Person Manager`} />
   </React.StrictMode>,
   document.getElementById("root")
 );
-// big changes!!
