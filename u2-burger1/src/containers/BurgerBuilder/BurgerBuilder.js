40cab886f485bdbc48cde8a0e63dcaa9f952455c
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2021-02-17 06:23:06 +0100

    add buger app

---
+++ u2-burger1/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+class BurgerBuilder extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <div>Burger</div>
+        <div>Burger Builder</div>
+      </Fragment>
+    );
+  }
+}
+
+export default BurgerBuilder;
