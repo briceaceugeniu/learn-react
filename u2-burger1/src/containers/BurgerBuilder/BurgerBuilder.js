d1016c417b94b18d9c2fd7209a1f50e4cbd33bce
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2021-02-18 10:18:31 +0100

    les 130

--- u2-burger1/src/containers/BurgerBuilder/BurgerBuilder.js
+++ u2-burger1/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,20 @@
 import React, { Fragment } from "react";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 2,
+      meat: 1,
+      beacon: 1,
+    },
+  };
+
   render() {
     return (
       <Fragment>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Builder</div>
       </Fragment>
     );
