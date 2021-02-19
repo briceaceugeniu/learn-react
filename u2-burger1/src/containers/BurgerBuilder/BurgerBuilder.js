ef13c266b8511d221151c90165fa2f842ab5c4ea
Briceac Eugeniu <eb@h4h.cc>
2021-02-19 15:23:26 +0100

    les 133

--- u2-burger1/src/containers/BurgerBuilder/BurgerBuilder.js
+++ u2-burger1/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Fragment } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICES = {
+  salad: 0.9,
+  cheese: 1.2,
+  meat: 2.3,
+  beacon: 1.8,
+};
+
 class BurgerBuilder extends React.Component {
   state = {
     ingredients: {
@@ -10,13 +17,44 @@ class BurgerBuilder extends React.Component {
       meat: 0,
       beacon: 0,
     },
+    totalPrice: 2.5,
+  };
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+  };
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+
+    const priceMinus = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceMinus;
+
+    this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
   };
 
   render() {
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Fragment>
     );
   }
